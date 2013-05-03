c51fa6c46bb52e787dccd51dd4e07387f2bfb213
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-05-03 10:39:16 +0100

    PEP8

--- gapy/client.py
+++ gapy/client.py
@@ -8,116 +8,130 @@ from gapy.error import GapyError
 
 GOOGLE_API_SCOPE = "https://www.googleapis.com/auth/analytics"
 
-def service_account(account_name, private_key=None, private_key_path=None, storage=None, storage_path=None):
-  """Create a client for a service account.
 
-   Args:
-    account_name: str, the account identifier (probably the account email).
-    private_key: str, the private key as a string.
-    private_key_path: str, path to a file with the private key in.
-    storage: oauth2client.client.Storage, a Storage implementation to store credentials.
-    storage_path: str, path to a file storage.
-  """
-  if not private_key:
-    if not private_key_path:
-      raise GapyError("Must provide either a private_key or a private_key_file")
-    if isinstance(private_key_path, basestring):
-      private_key_path = open(private_key_path)
-    private_key = private_key_path.read()
+def service_account(account_name, private_key=None, private_key_path=None,
+                    storage=None, storage_path=None):
+    """Create a client for a service account.
 
-  if not storage:
-    if not storage_path:
-      raise GapyError("Must provide either a storage object or a storage_path")
-    storage = Storage(filename=storage_path)
+     Args:
+      account_name: str, the account identifier (probably the account email).
+      private_key: str, the private key as a string.
+      private_key_path: str, path to a file with the private key in.
+      storage: oauth2client.client.Storage, a Storage implementation to store
+               credentials.
+      storage_path: str, path to a file storage.
+    """
+    if not private_key:
+        if not private_key_path:
+            raise GapyError(
+                "Must provide either a private_key or a private_key_file")
+        if isinstance(private_key_path, basestring):
+            private_key_path = open(private_key_path)
+        private_key = private_key_path.read()
 
-  credentials = SignedJwtAssertionCredentials(account_name, private_key, GOOGLE_API_SCOPE)
-  credentials.set_store(storage)
+    if not storage:
+        if not storage_path:
+            raise GapyError(
+                "Must provide either a storage object or a storage_path")
+        storage = Storage(filename=storage_path)
 
-  http = httplib2.Http()
-  http = credentials.authorize(http)
+    credentials = SignedJwtAssertionCredentials(account_name, private_key,
+                                                GOOGLE_API_SCOPE)
+    credentials.set_store(storage)
 
-  return Client(_build(http))
+    http = httplib2.Http()
+    http = credentials.authorize(http)
+
+    return Client(_build(http))
+
 
 def _build(http):
-  """Build the client object."""
-  return build("analytics", "v3", http=http)
+    """Build the client object."""
+    return build("analytics", "v3", http=http)
+
 
 class Client(object):
-  def __init__(self, service):
-    self._service = service
+    def __init__(self, service):
+        self._service = service
 
-  @property
-  def management(self):
-    return ManagementClient(self._service)
+    @property
+    def management(self):
+        return ManagementClient(self._service)
 
-  @property
-  def query(self):
-    return QueryClient(self._service)
+    @property
+    def query(self):
+        return QueryClient(self._service)
+
 
 class ManagementClient(object):
-  def __init__(self, service):
-    self._service = service
+    def __init__(self, service):
+        self._service = service
 
-  def accounts(self):
-      return self._list("accounts")
+    def accounts(self):
+        return self._list("accounts")
 
-  def account(self, id):
-    return self._item(self.accounts(), id)
+    def account(self, id):
+        return self._item(self.accounts(), id)
 
-  def webproperties(self, account):
-    return self._list("webproperties", accountId=account)
+    def webproperties(self, account):
+        return self._list("webproperties", accountId=account)
 
-  def webproperty(self, account, id):
-    return self._item(self.webproperties(account), id)
+    def webproperty(self, account, id):
+        return self._item(self.webproperties(account), id)
 
-  def profiles(self, account, webproperty):
-    return self._list("profiles", accountId=account, webPropertyId=webproperty)
+    def profiles(self, account, webproperty):
+        return self._list("profiles", accountId=account,
+                          webPropertyId=webproperty)
 
-  def profile(self, account, webproperty, id):
-    return self._item(self.profiles(account, webproperty), id)
+    def profile(self, account, webproperty, id):
+        return self._item(self.profiles(account, webproperty), id)
 
-  def _list(self, name, **kwargs):
-    return ManagementResponse(
-      getattr(self._service.management(), name)().list(**kwargs).execute()
-    )
+    def _list(self, name, **kwargs):
+        return ManagementResponse(
+            getattr(self._service.management(), name)().list(
+                **kwargs).execute()
+        )
 
-  def _item(self, response, id):
-    for item in response:
-      if item["id"] == id:
-        return item
-    raise GapyError("Id not found")
+    def _item(self, response, id):
+        for item in response:
+            if item["id"] == id:
+                return item
+        raise GapyError("Id not found")
+
 
 class QueryClient(object):
-  def __init__(self, service):
-    self._service = service
+    def __init__(self, service):
+        self._service = service
 
-  def get(self, ids, start_date, end_date, metrics, dimensions=None):
-    if not isinstance(ids, list):
-      ids = [ids]
-    if not isinstance(metrics, list):
-      metrics = [metrics]
-    start_date = start_date.strftime("%Y-%m-%d")
-    end_date = end_date.strftime("%Y-%m-%d")
-    if dimensions:
-      if not isinstance(dimensions, list):
-        dimensions = [dimensions]
-    else:
-      dimensions = []
+    def get(self, ids, start_date, end_date, metrics, dimensions=None):
+        if not isinstance(ids, list):
+            ids = [ids]
+        if not isinstance(metrics, list):
+            metrics = [metrics]
+        start_date = start_date.strftime("%Y-%m-%d")
+        end_date = end_date.strftime("%Y-%m-%d")
+        if dimensions:
+            if not isinstance(dimensions, list):
+                dimensions = [dimensions]
+        else:
+            dimensions = []
 
-    return self._get_response(metrics, dimensions,
-      ids=",".join("ga:%s" % id for id in ids),
-      start_date=start_date,
-      end_date=end_date,
-      metrics=",".join("ga:%s" % metric for metric in metrics),
-      dimensions=",".join("ga:%s" % dimension for dimension in dimensions)
-    )
+        return self._get_response(metrics, dimensions,
+                                  ids=",".join("ga:%s" % id for id in ids),
+                                  start_date=start_date,
+                                  end_date=end_date,
+                                  metrics=",".join(
+                                      "ga:%s" % metric for metric in metrics),
+                                  dimensions=",".join(
+                                      "ga:%s" % dimension for dimension in
+                                      dimensions))
 
-  def get_raw_response(self, **kwargs):
-    return self._service.data().ga().get(**kwargs).execute()
+    def get_raw_response(self, **kwargs):
+        return self._service.data().ga().get(**kwargs).execute()
 
-  def _get_response(self, m, d, **kwargs):
-    return QueryResponse(
-      self,
-      self.get_raw_response(**kwargs),
-      m, d
-    )
+    def _get_response(self, m, d, **kwargs):
+        return QueryResponse(
+            self,
+            self.get_raw_response(**kwargs),
+            m, d
+        )
--- setup.py
+++ setup.py
@@ -2,39 +2,39 @@ import os
 import sys
 
 try:
-  from setuptools import setup
+    from setuptools import setup
 except ImportError:
-  from distutils.core import setup
+    from distutils.core import setup
 
 if sys.argv[-1] == "publish":
-  os.system("python setup.py sdist upload")
-  sys.exit()
+    os.system("python setup.py sdist upload")
+    sys.exit()
 
 packages = [
-  "gapy"
+    "gapy"
 ]
 
 requires = [
-  "google-api-python-client==1.0",
-  "pyOpenSSL==0.13"
+    "google-api-python-client==1.0",
+    "pyOpenSSL==0.13"
 ]
 
 setup(
-  name="gapy",
-  version="0.0.2",
-  description="Painless Google Analytics",
-  author="Rob Young",
-  url="https://github.com/robyoung/gapy",
-  packages=packages,
-  package_dir={"gapy":"gapy"},
-  include_package_data=True,
-  install_requires=requires,
-  license="https://raw.github.com/robyoung/gapy/master/LICENSE",
-  classifiers=[
-    "Development Status :: 3 - Alpha",
-    "Intended Audience :: Developers",
-    "Natural Language :: English",
-    "Programming Language :: Python",
-    "License :: OSI Approved :: MIT License"
-  ]
+    name="gapy",
+    version="0.0.2",
+    description="Painless Google Analytics",
+    author="Rob Young",
+    url="https://github.com/robyoung/gapy",
+    packages=packages,
+    package_dir={"gapy": "gapy"},
+    include_package_data=True,
+    install_requires=requires,
+    license="https://raw.github.com/robyoung/gapy/master/LICENSE",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License"
+    ]
 )
